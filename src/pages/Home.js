119c8cf5b5fd0589b8bf59fdf7b2a9b57aa8c9a7
Warren-Tan <warrentanjk@yahoo.com>
2026-01-19 10:34:19 +0800

    warren's thing

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,8 +2,20 @@ import { Link } from "react-router-dom";
 
 export default function Home() {
   /* TODO: Design and complete the Home page
-    - display instructions
-    - link to Cards page
-    - style as a landing page */
+    - display instructions*/
+
+    /*- link to Cards page*/
+  <>
+    <p>Welcome to the Card App! This is a simple application for managing cards.</p>
+    
+    <p>Click here to add new cards to the cards list:</p>
+    <Link to="/cards/new">Add New Card</Link>
+
+    <p>Click here to view your cards:</p>
+    <Link to="/cards">View Cards</Link>
+  </>
+    /*- style as a landing page */
+  
+
   return <main></main>;
 }
